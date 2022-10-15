dfbccf9faf22a0ee4a635b5d49c348b5c3e12b47
Kirill <kirillpugach2@gmail.com>
2022-10-15 12:21:30 +0300

    Finish

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Banner();
                 }
             }
         }
@@ -30,14 +36,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Banner(){
+    val image= painterResource(id = R.drawable.ic_task_completed);
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Image(painter = image, contentDescription = null)
+        Text(text = stringResource(R.string.all_task_C),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top=24.dp, bottom = 8.dp)
+        )
+        Text(text = stringResource(R.string.N_work), fontSize = 16.sp)
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        Banner();
     }
 }
